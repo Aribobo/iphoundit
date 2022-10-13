a313b7ac4a253e241f2d4d8fdc279041876b9bb9
Aribobo <stanleyemmanuel04@gmail.com>
2022-10-13 11:24:27 +0100

    view added

--- pages/estate/[id].js
+++ pages/estate/[id].js
@@ -36,26 +36,33 @@ const Estate = ({ estates }) => {
   return (
     <div id="rent" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16 justify-center">
-        {/* <p className=" p-2 text-xl tracking items-center uppercase text-[#fd7e14] text-bold text-center mt-10">
-          Properties
-        </p> */}
 
         <div className="grid rows-3 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-8">
           {estates.map((estate) => (
             <div
-              className=" bg-[#e5e7eb] shadow-gray-600 rounded-xl overflow-hidden shadow-xl"
+              className="  relative group hover:bg-gradient-to-r from-[#e5e7eb] to-[#1e1e20] opacity-1 bg-[#e5e7eb] shadow-gray-600 rounded-xl overflow-hidden shadow-xl"
               key={estate._id}
             >
               <Link href={`/desc/${estate._id}`}>
                 <Image
                   className="w-full shadow 
-                 group-hover:opacity-10 object-fill hover:scale-105 ease in duration-300 md:object-fill"
+                 group-hover:opacity-10 object-fill hover:scale-105 ease in duration-300 md:object-fill cursor-pointer"
                   src={estate.image}
                   alt=" "
                   width={900}
               height={600} 
                 />
               </Link>
+              <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+      
+        <Link href={`/desc/${estate._id}`}>
+          <p className="text-center py-3  font-bold underline cursor-pointer">
+          <button className="px-8 py-2 mt-4 mr-8 bg-[#2b2a2a] text-[#e5e7eb] font-bold rounded mb-10">
+                  View
+                </button>
+          </p>
+        </Link>
+      </div>
               <div className="p-6  rounded-xl ">
                 <h4 className="text-xl text-black tracking-wider pb-2 ">
                   {estate.title}
